e50929ceb94a9245351e23f5daf8496e5d2dddf0
adpsxx <ranupurditya@gmail.com>
2026-09-09 19:49:19 +0700

    Feat: Add new models (Project) and temporary page displaying the data.

--- main/models.py
+++ main/models.py
@@ -28,3 +28,17 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    project_link = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_ongoing(self):
+        return self.ended_at is None
