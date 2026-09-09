13ef105129877e85e0810b5b3c1c5f36fcbc5424
adpsxx <ranupurditya@gmail.com>
2026-09-09 20:14:49 +0700

    Add CSFR_TRUSTED_ORIGINS to fix pws deployment issue

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -17,6 +17,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "andranu-dhawy-myportofolio.pws.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["https://andranu-dhawy-myportofolio.pws.cs.ui.ac.id"]
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
