24a8486d6810c9cbaa060cb9efe5014fc9b8ec3d
adpsxx <ranupurditya@gmail.com>
2026-09-16 16:20:59 +0700

    Create show_project, delete_project, and get_projects_json function.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_project, create_project
+from main.views import show_main, show_experience, show_project, create_project, get_projects_json, delete_project
 
 app_name = "main"
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("projects/", show_project, name="show_project"),
-    path("projects/add/", create_project, name="create_project")
+    path("projects/add/", create_project, name="create_project"),
+    path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project"),
+    path("api/projects/", get_projects_json, name="get_projects_json")
 ]
--- main/views.py
+++ main/views.py
@@ -31,9 +31,19 @@ def show_experience(request):
     return render(request, "experience.html", context)
 
 def show_project(request):
+    json_response = get_projects_json(request)
+
+    projects = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    projects = [project.object for project in projects]
+    title_query = request.GET.get("title", "").strip()
+
     context = {
         "name": "Andranu",
-        "project_list": Project.objects.all(),
+        "project_list": projects,
+        "title_query": title_query,
     }
     return render(request, "project.html", context)
 
@@ -50,3 +60,23 @@ def create_project(request):
         "form": form,
     }
     return render(request, "projects_form.html", context)
+
+def delete_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+
+    if request.method == "POST":
+        project.delete()
+        messages.success(request, "Project berhasil dihapus!")
+        return redirect("main:show_projects")
+
+    return redirect("main:show_projects")
+
+def get_projects_json(request):
+    title_query = request.GET.get("title", "").strip()
+    projects = Project.objects.all()
+
+    if title_query:
+        projects = projects.filter(title__icontains=title_query)
+
+    projects_json = serializers.serialize("json", projects)
+    return HttpResponse(projects_json, content_type="application/json")
