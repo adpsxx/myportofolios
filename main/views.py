3da18bce37900da61677f45c1d108a728caf18c1
adpsxx <ranupurditya@gmail.com>
2026-09-16 17:43:42 +0700

    Make functionality for the delete button

--- main/views.py
+++ main/views.py
@@ -67,9 +67,9 @@ def delete_project(request, project_id):
     if request.method == "POST":
         project.delete()
         messages.success(request, "Project berhasil dihapus!")
-        return redirect("main:show_projects")
+        return redirect("main:show_project")
 
-    return redirect("main:show_projects")
+    return redirect("main:show_project")
 
 def get_projects_json(request):
     title_query = request.GET.get("title", "").strip()
